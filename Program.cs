a19e718ff41ee81d2934b4d385f65436207a78f7
David Alejandro García Garcia <AL07106842@tecmilenio.mx>
2025-07-18 18:27:43 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace C_Sharp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to DAGG C# 9 Catalog");
+            Console.WriteLine("This catalog contains examples of C# 9.0 features");
+            Console.WriteLine("These features are available in .NET Core 3.0+");
+
+
+        }
+    }
+}
